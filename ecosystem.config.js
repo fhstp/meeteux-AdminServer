c015ec706f8a9934cc632118b877c7659dad2a2a
Schlese <niklas.thuer@gmail.com>
2018-10-03 09:27:46 +0200

    Updated auto deployment

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,11 +9,13 @@ module.exports = {
         }
     ],
     deploy: {
-        user: 'prod',
-        host: 'god.meeteux.fhstp.ac.at',
-        ref: 'origin/master',
-        repo: "https://github.com/fhstp/meeteux-AdminServer.git",
-        path: '/srv/adminPanel',
-        'post-deploy': 'cp ../.env ./ && npm install && pm2 startOrRestart ecosystem.config.js --env production'
+        production: {
+            user: 'prod',
+            host: 'god.meeteux.fhstp.ac.at',
+            ref: 'origin/master',
+            repo: "https://github.com/fhstp/meeteux-AdminServer.git",
+            path: '/srv/adminPanel',
+            'post-deploy': 'cp ../.env ./ && npm install && npm run tsc && pm2 startOrRestart ecosystem.config.js --env production'
+        }
     }
 };
