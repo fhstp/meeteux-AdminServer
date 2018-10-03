e60e975e0f75270dc9069e71d328a8336a8588ba
Schlese <niklas.thuer@gmail.com>
2018-10-03 09:29:40 +0200

    Added develop auto deployment

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -16,6 +16,14 @@ module.exports = {
             repo: "https://github.com/fhstp/meeteux-AdminServer.git",
             path: '/srv/adminPanel',
             'post-deploy': 'cp ../.env ./ && npm install && npm run tsc && pm2 startOrRestart ecosystem.config.js --env production'
+        },
+        develop: {
+            user: 'prod',
+            host: 'god.meeteux.fhstp.ac.at',
+            ref: 'origin/develop',
+            repo: "https://github.com/fhstp/meeteux-AdminServer.git",
+            path: '/srv/adminPanel',
+            'post-deploy': 'cp ../.env ./ && npm install && npm run tsc && pm2 startOrRestart ecosystem.config.js --env production'
         }
     }
 };
